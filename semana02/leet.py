9acad0facd282879c404f9244f1868447d51a115
Andre Almeida <andre.almeida@escolar.ifrn.edu.br>
2021-08-12 12:48:39 +0000

    Resolução dos Exercícios de Sala

--- semana02/leet.py
+++ semana02/leet.py
@@ -1,5 +1,6 @@
 import re
 def traduzir(palavra):
+    #Cria um dicionário com os caracteres
     alfabetoLeet={
         'A':'4',
         'E':'3',
@@ -7,8 +8,9 @@ def traduzir(palavra):
         'O':'0',
         'U':'(_)'
     }
-    
+    #Usa um for para iterar sobre o dicionário
     for letra,caractereLeet in alfabetoLeet.items():
+        #O método sub da lib re faz a substituição, ignorando o case
        palavra=re.sub(letra,caractereLeet,palavra,flags=re.IGNORECASE)
     return palavra
 
